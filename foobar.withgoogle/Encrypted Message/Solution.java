d21a5526ec879ad5387c71b46194d8c0f8f90926
Toan Dinh <huutoan.dnh@gmail.com>
2023-05-30 02:02:42 -0400

    fixed indentation

--- foobar.withgoogle/Encrypted Message/Solution.java
+++ foobar.withgoogle/Encrypted Message/Solution.java
@@ -4,7 +4,7 @@ public class Solution {
     public static void main(String args[]) {
         String encrypted = "AkgGBBYCCxdBFllVVVYSEwsFRhZVT1ISGg0CAVNWDApSUU9BSQFBRRwKGBQRRkJEFVQfCRoDARJJ RAgRXgYbEgcECg1QXRxIWVFSAA0MW1QPChgUGxVJRAgRXhobHRoCBQFWFlVPUgMUAwwNRkJeT09R UhIPAlcWVU9SFxoOSUQIEV4YHB9URhM=";
         final byte[] ct = encrypted.replaceAll("\\s","").getBytes();
-		final byte[] key = "youquand21".getBytes();
+	final byte[] key = "youquand21".getBytes();
 		
         byte[] decoded = Base64.getDecoder().decode(ct);
         byte[] pt = new byte[decoded.length];
